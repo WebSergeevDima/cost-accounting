adeebca66758053df818ea306559ba5dab28a0b5
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-07-26 22:19:29 +0300

    add cost-item component and add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import CostItem from "./components/CostItem";
+
+
 function App() {
     return (
         <div className="App">
             <h1>React</h1>
+            <CostItem/>
         </div>
     );
 }
---
+++ src/components/CostItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CostItem.css';
+
+const CostItem = () => {
+    return (
+        <div className="cost-item">
+            <div>Дата</div>
+            <div>
+                <div>Заголовок</div>
+                <div>999.99</div>
+            </div>
+        </div>
+    );
+};
+
+export default CostItem;
