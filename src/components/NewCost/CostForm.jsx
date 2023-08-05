c8cd590868223c293b55451b7d8cbcb7b8f007cc
Sergeev Dima <web.sergeev.dima@gmail.com>
2023-08-05 10:28:11 +0300

    update cost form

--- src/components/NewCost/CostForm.jsx
+++ src/components/NewCost/CostForm.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CostForm.css';
 
 const CostForm = () => {
+
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const changeNameHandler = (event) => {
+        setName(event.target.value)
+    }
+
+    const changeAmountHandler = (event) => {
+        setAmount(event.target.value)
+    }
+
+    const changeDateHandler = (event) => {
+        setDate(event.target.value)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const costData = {
+            name,
+            amount,
+            date: new Date(date)
+        };
+
+        setName('');
+        setAmount('');
+        setDate('');
+    }
+
     return (
         <div className="cost-form">
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className="cost-form__control">
                     <label>Название</label>
-                    <input type="text"/>
+                    <input type="text" onChange={changeNameHandler} value={name}/>
                 </div>
                 <div className="cost-form__control">
-                    <label>Название</label>
-                    <input type="number" step="0.01" min="0.01"/>
+                    <label>Сумма</label>
+                    <input type="number" step="0.01" min="0.01" onChange={changeAmountHandler} value={amount}/>
                 </div>
                 <div className="cost-form__control">
-                    <label>Название</label>
-                    <input type="date" max="2023-08-02"/>
+                    <label>Дата</label>
+                    <input type="date" max="2023-08-02" onChange={changeDateHandler} value={date}/>
                 </div>
-                <div type="submit" className="cost-form__actions">
+                <button type="submit" className="cost-form__actions">
                     <div className="cost-form__btn">Добавить расход</div>
-                </div>
+                </button>
             </form>
         </div>
     );
